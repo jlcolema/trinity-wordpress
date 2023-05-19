b05529d48ac5d0bde1fbf538ec6d110f40f516c0
Joshua Coleman <jlcolema@icloud.com>
2023-05-19 15:41:04 -0500

    Remove backward compatibility check.

--- app/public/wp-content/themes/trinity/functions.php
+++ app/public/wp-content/themes/trinity/functions.php
@@ -26,11 +26,6 @@
  *
  */
 
-// This theme requires WordPress 5.3 or later.
-if ( version_compare( $GLOBALS['wp_version'], '5.3', '<' ) ) {
-	require get_template_directory() . '/inc/back-compat.php';
-}
-
 if ( ! function_exists( 'trinity_setup' ) ) {
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
