d1d13c126261af89e4c1c1b8803fdc646279947a
Joshua Coleman <jlcolema@icloud.com>
2023-05-19 17:21:04 -0500

    Connect scripts to the template.

--- app/public/wp-content/themes/trinity/footer.php
+++ app/public/wp-content/themes/trinity/footer.php
@@ -39,7 +39,7 @@
 
 	<?php wp_footer(); ?>
 
-	<script src="/assets/js/scripts.js" defer></script>
+	<script src="<?php bloginfo( 'template_directory' ); ?>/assets/js/scripts.js" defer></script>
 
 </body>
 
