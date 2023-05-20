f270aba921388e4fff5d002017c59829c6b31ac4
Joshua Coleman <jlcolema@icloud.com>
2023-05-20 12:13:58 -0500

    Connect the Trusted section to the system.

--- app/public/wp-content/themes/trinity/includes/trusted.php
+++ app/public/wp-content/themes/trinity/includes/trusted.php
@@ -1,124 +1,128 @@
 <section class="section section__trusted">
 
-	<header class="section__header">
+	<?php
 
-		<h3 class="section__heading">Trusted By:</h3>
+		$trusted = get_field('trusted');
 
-	</header>
+	?>
 
-	<div class="carousel__container">
+	<?php if ( $trusted ) : ?>
 
-		<div class="glide hero">
+		<header class="section__header">
 
-			<div class="glide__track" data-glide-el="track">
+			<h3 class="section__heading"><?php echo $trusted['trusted_heading']; ?></h3>
 
-				<ul class="trusted__list glide__slides">
+		</header>
 
-					<li class="trusted__item glide__slide">
+		<?php
 
-						<img src="/media/logo-01.png" srcset="/media/logo-01.png, /media/logo-01@2x.png" width="186" height="97" alt="Company 01" loading="lazy" class="trusted__logo" />
+			$trusted_companies = $trusted['trusted_companies'];
 
-					</li>
+		?>
 
-					<li class="trusted__item glide__slide">
+		<?php if ( $trusted_companies ) : ?>
 
-						<img src="/media/logo-02.png" srcset="/media/logo-02.png, /media/logo-02@2x.png" width="186" height="97" alt="Company 02" loading="lazy" class="trusted__logo" />
+			<div class="carousel__container">
 
-					</li>
+				<div class="glide hero">
 
-					<li class="trusted__item glide__slide">
+					<div class="glide__track" data-glide-el="track">
 
-						<img src="/media/logo-03.png" srcset="/media/logo-03.png, /media/logo-03@2x.png" width="186" height="97" alt="Company 03" loading="lazy" class="trusted__logo" />
+						<ul class="trusted__list glide__slides">
 
-					</li>
+							<?php foreach( $trusted_companies as $post ) : ?>
 
-					<li class="trusted__item glide__slide">
+								<?php setup_postdata( $post ); ?>
 
-						<img src="/media/logo-04.png" srcset="/media/logo-04.png, /media/logo-04@2x.png" width="186" height="97" alt="Company 04" loading="lazy" class="trusted__logo" />
+								<?php
 
-					</li>
+									$company_logo = get_field('company_logo');
 
-					<li class="trusted__item glide__slide">
+									// Attributes
+									$url = $company_logo['url'];
+									$alt = $company_logo['alt'];
 
-						<img src="/media/logo-05.png" srcset="/media/logo-05.png, /media/logo-05@2x.png" width="186" height="97" alt="Company 05" loading="lazy" class="trusted__logo" />
+									// Size Options
+									$small = 'company-logo-small'; // 186x97
+									$large = 'company-logo-large'; // 372x914
 
-					</li>
+									// Sizes to Use
+									$logo_small = $company_logo['sizes'][$small];
+									$logo_large = $company_logo['sizes'][$large];
 
-					<li class="trusted__item glide__slide">
+								?>
 
-						<img src="/media/logo-06.png" srcset="/media/logo-06.png, /media/logo-06@2x.png" width="186" height="97" alt="Company 06" loading="lazy" class="trusted__logo" />
+								<li class="trusted__item glide__slide">
 
-					</li>
+									<img src="<?php echo esc_url($logo_small); ?>" srcset="<?php echo esc_url($logo_small); ?>, <?php echo esc_url($logo_large); ?> 2x" width="186" height="97" alt="<?php the_title(); ?>" loading="lazy" class="trusted__logo" />
 
-					<li class="trusted__item glide__slide">
+								</li>
 
-						<img src="/media/logo-07.png" srcset="/media/logo-07.png, /media/logo-07@2x.png" width="186" height="97" alt="Company 07" loading="lazy" class="trusted__logo" />
+							<?php endforeach; ?>
 
-					</li>
+						</ul>
 
-					<li class="trusted__item glide__slide">
+					</div>
 
-						<img src="/media/logo-08.png" srcset="/media/logo-08.png, /media/logo-08@2x.png" width="186" height="97" alt="Company 08" loading="lazy" class="trusted__logo" />
+					<div class="glide__arrows" data-glide-el="controls">
 
-					</li>
+						<button class="glide__arrow glide__arrow--prev" data-glide-dir="<">
 
-				</ul>
+							<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24.63 45.02">
+								<polygon points="22.51 0 24.63 2.12 4.24 22.51 24.63 42.9 22.51 45.02 0 22.51 22.51 0" style="fill: var(--color-theme-light-blue)" />
+							</svg>
+
+							<span class="label visually-hidden">Previous</span>
+
+						</button>
+
+						<button class="glide__arrow glide__arrow--next" data-glide-dir=">">
+
+							<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24.63 45.02">
+								<polygon points="2.12 45.02 0 42.9 20.39 22.51 0 2.12 2.12 0 24.63 22.51 2.12 45.02" style="fill: var(--color-theme-light-blue)" />
+							</svg>
+
+							<span class="label visually-hidden">Next</span>
+
+						</button>
+
+					</div>
+
+					<div class="glide__bullets" data-glide-el="controls[nav]">
+						<button class="glide__bullet" data-glide-dir="=0">
+							<span class="visually-hidden">1</span>
+						</button>
+						<button class="glide__bullet" data-glide-dir="=1">
+							<span class="visually-hidden">2</span>
+						</button>
+						<button class="glide__bullet" data-glide-dir="=2">
+							<span class="visually-hidden">3</span>
+						</button>
+						<button class="glide__bullet" data-glide-dir="=3">
+							<span class="visually-hidden">4</span>
+						</button>
+						<button class="glide__bullet" data-glide-dir="=4">
+							<span class="visually-hidden">5</span>
+						</button>
+						<button class="glide__bullet" data-glide-dir="=5">
+							<span class="visually-hidden">6</span>
+						</button>
+						<button class="glide__bullet" data-glide-dir="=6">
+							<span class="visually-hidden">7</span>
+						</button>
+						<button class="glide__bullet" data-glide-dir="=7">
+							<span class="visually-hidden">8</span>
+						</button>
+					</div>
+
+				</div>
 
 			</div>
 
-			<div class="glide__arrows" data-glide-el="controls">
+			<?php wp_reset_postdata(); ?>
 
-				<button class="glide__arrow glide__arrow--prev" data-glide-dir="<">
+		<?php endif; ?>
 
-					<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24.63 45.02">
-						<polygon points="22.51 0 24.63 2.12 4.24 22.51 24.63 42.9 22.51 45.02 0 22.51 22.51 0" style="fill: var(--color-theme-light-blue)" />
-					</svg>
-
-					<span class="label visually-hidden">Previous</span>
-
-				</button>
-
-				<button class="glide__arrow glide__arrow--next" data-glide-dir=">">
-
-					<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24.63 45.02">
-						<polygon points="2.12 45.02 0 42.9 20.39 22.51 0 2.12 2.12 0 24.63 22.51 2.12 45.02" style="fill: var(--color-theme-light-blue)" />
-					</svg>
-
-					<span class="label visually-hidden">Next</span>
-
-				</button>
-
-			</div>
-
-			<div class="glide__bullets" data-glide-el="controls[nav]">
-				<button class="glide__bullet" data-glide-dir="=0">
-					<span class="visually-hidden">1</span>
-				</button>
-				<button class="glide__bullet" data-glide-dir="=1">
-					<span class="visually-hidden">2</span>
-				</button>
-				<button class="glide__bullet" data-glide-dir="=2">
-					<span class="visually-hidden">3</span>
-				</button>
-				<button class="glide__bullet" data-glide-dir="=3">
-					<span class="visually-hidden">4</span>
-				</button>
-				<button class="glide__bullet" data-glide-dir="=4">
-					<span class="visually-hidden">5</span>
-				</button>
-				<button class="glide__bullet" data-glide-dir="=5">
-					<span class="visually-hidden">6</span>
-				</button>
-				<button class="glide__bullet" data-glide-dir="=6">
-					<span class="visually-hidden">7</span>
-				</button>
-				<button class="glide__bullet" data-glide-dir="=7">
-					<span class="visually-hidden">8</span>
-				</button>
-			</div>
-
-		</div>
-
-	</div>
+	<?php endif; ?>
 
 </section>
